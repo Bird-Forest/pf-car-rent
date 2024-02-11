ee8d385bc354356b7a4bbaa2a6b904dc7aa1ea2d
Bird-Forest <bird.forest70@gmail.com>
2024-02-11 23:02:33 +0200

    added redux router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <BrowserRouter basename="/pf-car-rent"> */}
+    <BrowserRouter basename="/pf-car-rent">
       <App />
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
