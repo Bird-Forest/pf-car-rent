06d79c4e3ee9fa3d1d6616cd09d6edec284cd4c5
Bird-Forest <127691003+Bird-Forest@users.noreply.github.com>
2024-02-15 19:46:35 +0000

    Deploying to gh-pages from @ Bird-Forest/pf-car-rent@371d0cf6275eba37897674daefbdc7c344d651ce 🚀

--- static/js/697.c0dd1d01.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[697],{5697:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});r(2791);var n=r(184);function a(){return(0,n.jsx)("div",{children:"FavoritesPage"})}}}]);
-//# sourceMappingURL=697.c0dd1d01.chunk.js.map
